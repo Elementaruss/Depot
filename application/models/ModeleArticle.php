490f7df594bf9e7e4ee495aaaf3b5ff0939522c2
Elementaruss <alex.ben2227@gmail.com>
2018-04-11 10:50:45 +0200

    Marchand en dvp

--- application/models/ModeleArticle.php
+++ application/models/ModeleArticle.php
@@ -11,11 +11,11 @@ $this->load->database();
      {
         if ($pNoArticle === FALSE) // pas de n° d'article en paramètre
         {  // on retourne tous les articles
-             $requete = $this->db->get('tabarticle');
+             $requete = $this->db->get('produit');
              return $requete->result_array(); // retour d'un tableau associatif
         }
         // ici on va chercher l'article dont l'id est $pNoArticle
-        $requete = $this->db->get_where('tabarticle', array('cNo' => $pNoArticle));
+        $requete = $this->db->get_where('produit', array('noproduit' => $pNoArticle));
         return $requete->row_array(); // retour d'un tableau associatif
      } // fin retournerArticles
 
---
+++ application/views/administrateur/admin.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Acceuil Administrateur</title>
+    <link rel=stylesheet type="text/css" href="style.css"/>
+  </head>
+  <body>
+    <h1>Mise &agrave; jour des formations</h1>
+    <br><a href="inserform.php">Inserer des formations</a></br>
+    <br><a href="affichform.php">Afficher les formations</a></br>
+    <BR></BR><a href="deconnection.php"><input type="button" value="Changer d'utilisateur"/></a>
+  </body>
+</html>
